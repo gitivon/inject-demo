d182c2e9409f43aa9d9267c15517631c5b80f5c5
华清峰 <80506313@qq.com>
2018-11-04 00:28:39 +0800

    feat: add @Inject

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
 import 'reflect-metadata';
 
+const DESIGN_PARAMTYPES = 'design:paramtypes'
+
 const Injectable = () => (target: any) => {
-  Reflect.getMetadata('design:paramtypes', target);
+  Reflect.getMetadata(DESIGN_PARAMTYPES, target);
+}
+
+const Inject = (Foo: any) => (target: any, property: string, index: number) => {
+  const metadata = Reflect.getMetadata(DESIGN_PARAMTYPES, target, property) || [];
+  metadata[index] = Foo;
+  Reflect.metadata(DESIGN_PARAMTYPES, metadata)(target, property);
 }
 
 class Person {}
+class Male extends Person {}
 
 @Injectable()
 class User {
 
   constructor(public readonly person: Person) {}
+
+  public getInfo(@Inject(Male) man: Male) {
+    console.log(man);
+  }
 }
 
 const Factory = (target: any) => {
@@ -21,7 +34,11 @@ const Factory = (target: any) => {
 // 注入器添加 Provider，相当于 module 配置里的 providers
 const providers = [Person];
 
-// 一个实例化工厂
+// 一个实例化工厂，对构造函数进行诸如
 const user = Factory(User);
+console.log(user.person); // Person {}
 
-console.log(user.person);
+// 执行 getInfo 方法，并根据依赖进行注入
+const argFns = Reflect.getMetadata(DESIGN_PARAMTYPES, user, 'getInfo');
+const params = argFns.map((Fn: any) => new Fn());
+user.getInfo(...params); // Male {}
